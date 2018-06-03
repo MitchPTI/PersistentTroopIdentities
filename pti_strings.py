2444d365a40aaa6fb67dff792985f565ee355200
MitchPTI <mitchell.dale249@gmail.com>
2018-06-03 18:06:58 +1000

    Updated name strings to ensure no duplicates within factions

--- pti_strings.py
+++ pti_strings.py
@@ -13,10 +13,10 @@ for filename in [f for f in os.listdir("./names") if os.path.isfile("./names/" +
 strings = []
 for gender in ("boy", "girl"):
 	for faction, regions in faction_naming_regions.iteritems():
-		faction_names = []
+		faction_names = set([])
 		for region in regions:
-			faction_names.extend(names[(region, gender)])
+			faction_names.update(names[(region, gender)])
 		
-		strings.extend([("{}_{}_name_{}".format(faction, gender, i), name.strip()) for i, name in enumerate(faction_names)])
+		strings.extend([("{}_{}_name_{}".format(faction, gender, i), name.strip()) for i, name in enumerate(sorted(list(faction_names)))])
 		end_string = "{}_{}_names_end".format(faction, gender)
 		strings.append((end_string, "{!}" + end_string))
